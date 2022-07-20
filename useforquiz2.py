92d83461375776c59c4bba9bd9419e3c691ee0d6
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2022-07-20 15:04:28 +0900

    Create useforquiz2.py

---
+++ useforquiz2.py
@@ -0,0 +1,10 @@
+#quiz 2
+
+line=5 #몇줄짜리 사각형
+a=3       #반복횟수
+for count in range(a):  #반복횟수만큼 반복
+     for value in range(line):     
+          for j in range(line):
+               print("*"," ",end='')
+          print()
+     print()
