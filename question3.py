6de573f7f7f1f1a88389da39b09157dced9f9eb5
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2022-07-20 20:16:10 +0900

    Update question3.py

--- question3.py
+++ question3.py
@@ -2,13 +2,13 @@ import turtle, random
 
 a = turtle.Turtle()
 a.speed(0)
-colorlist = ['black',"pink","red","blue","gold","brown","aquamarine2","coral3","cyan2","hotpink","firebrick1","maroon2","RoyalBlue2","tomato","snow2"]
-
+colorlist = ['black',"pink","red","blue","gold","brown","aquamarine2","coral3","cyan2","hotpink","firebrick1","maroon2","RoyalBlue2","tomato","snow2","DeepSkyBlue","IndianRed2","magenta3","PaleTurquoise","salmon","wheat1","OliveDrab"]
+turtle.bgcolor('wheat4')
 for i in range(300):
     if i < 180:
         a.color(random.choice(colorlist))
         a.forward(i)
-        a.right(96)
+        a.right(96)   
     else:
         a.color(random.choice(colorlist))
         a.forward(i)
