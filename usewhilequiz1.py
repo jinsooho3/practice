182f6bd169f553cab9797d0d79e3a50953c8fe6d
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2022-07-20 15:04:34 +0900

    Create usewhilequiz1.py

---
+++ usewhilequiz1.py
@@ -0,0 +1,11 @@
+#quiz 1
+num =5 # int(input("양의 정수 입력 : "))
+value=1
+
+while value <= num:
+     i = 1
+     while i <= value:
+          print(i," ", end='')
+          i +=1
+     value +=1
+     print()
