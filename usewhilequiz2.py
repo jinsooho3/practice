23dca0790be75ed2b5b9b613400bd965552b5b62
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2022-07-20 15:04:36 +0900

    Create usewhilequiz2.py

---
+++ usewhilequiz2.py
@@ -0,0 +1,15 @@
+#quiz 2
+count=3
+row =5
+value = 5
+while count>0:
+     value=5
+     while value>0:
+          row=5
+          while row>0:
+               print("*",end='')
+               row -=1
+          print()
+          value -= 1
+     print()
+     count -=1
