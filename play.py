8577ac8fc55fb805a8432e69b5f773d16334eedd
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2022-08-08 09:50:11 +0900

    Create play.py

---
+++ play.py
@@ -0,0 +1,11 @@
+n = 5
+arr1 = [[0 for j in range(n)] for i in range(n)]
+arr2 = [[0] * n] * n
+arr3 = [[0] * n for i in range(n)]
+
+print(arr1)
+print(arr2)
+print(arr3)
+
+mylist = [10* 40]
+print(mylist)
