e8488c8057dfafcdedc54fc9d8f1544c42c53f78
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2022-08-08 20:47:19 +0900

    Create 0808_tupleFunction.py

---
+++ 0808_tupleFunction.py
@@ -0,0 +1,10 @@
+def sumAndMulti(numA, numB):
+    sum = numA + numB
+    multi = numA * numB
+    
+    return sum, multi
+
+a = 4
+b = 5
+sum, multi = sumAndMulti(a, b)
+print("두 수의 합은 : ",sum,", 두 수의 곱은 : ",multi)
