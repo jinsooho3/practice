c8b9e3566ee6b2f4d9ceee11b061fba86120c249
jinsooho3 <109533678+jinsooho3@users.noreply.github.com>
2022-08-12 10:39:39 +0900

    Update 0811_findingWord.py

--- 0811_findingWord.py
+++ 0811_findingWord.py
@@ -14,7 +14,7 @@ wordList = []
 while len(wordList) < 3:
     print(len(wordList) + 1, end = '')
     inputWord = input("번째 단어를 입력해주세요")
-    if 3 < len(inputWord) < 20 and inputWord not in wordList:
+    if 5 <= len(inputWord) <= 20 and inputWord not in wordList:
         wordList.append(inputWord)
     else:
         print("다시 입력해주세요")   
@@ -23,21 +23,36 @@ while len(wordList) < 3:
 randomWord = random.choice(wordList)
 underBarNum = []
 while len(underBarNum) < banUp(len(randomWord)):
-    randNum = random.randint(0, len(randomWord))
+    randNum = random.randint(0, len(randomWord) - 1)
     if randNum not in underBarNum:
         underBarNum.append(randNum)
 
+tryCount = 0
 while True:
+    tryCount += 1
+
+    print(tryCount,"번째 시도, 아래단어를 구성하는 알파벳 한개를 입력하세요.")
+
     for index in range(len(randomWord)):
         if index in underBarNum:
             print("_", end = '')
         else:
             print(randomWord[index], end = '')
     print()
-    inputChar = input("알파벳 입력하세요 :")
-    for count in underBarNum:
-        if randomWord[count] == inputChar:
-            underBarNum.remove(count)
+    correctChar = []
+    
+    inputChar = input()
+    if inputChar in randomWord:
+        for count in underBarNum:
+            if randomWord[count] == inputChar:
+                correctChar.append(count)
+    else:
+        print("단어 내 포함되지 않은 알파벳입니다.")
+
+    
+    for index in correctChar:
+        underBarNum.remove(index)
+
     if len(underBarNum) == 0:
-        print(randomWord)
+        print("Clear - 선택된 단어 : ",randomWord,", 총 시도 횟수 : ",tryCount)
         break
